605315448ab914fadb811e47fac30d7c30b44616
Cs-TiGA <bi55qh@student.sunderland.ac.uk>
2025-01-11 19:24:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -146,5 +146,5 @@ def openai_generate(prompt, role):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.getenv("PORT", 4000))  # Default to 5000 if PORT is not set
     app.run(host='0.0.0.0', port=port)
